6d2ac959e833a8019875c07f613c99ff042e3218
Vitaly Lobchuk <vitaly@lobchuk.ru>
2016-12-05 10:24:04 +0300

    Fix comment

--- middleware/alias.go
+++ middleware/alias.go
@@ -2,5 +2,5 @@ package middleware
 
 import "github.com/l-vitaly/stringsvc"
 
-// Alias Middleware
+// Alias Middleware for func(stringsvc.StringSvc) stringsvc.StringSvc
 type Middleware func(stringsvc.StringSvc) stringsvc.StringSvc
